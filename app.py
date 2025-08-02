e0c763ca4cd33d5c00320135f079d58925370d03
Jorge Adrian Iglesias <50002132+Adrian-Iglesias-1@users.noreply.github.com>
2025-08-02 16:50:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from openpyxl.utils import get_column_letter
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT',5000))
     st.run()
 
 # Configuración de la página
